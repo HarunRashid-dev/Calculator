fcaaea3816e65d9cdd1a11b718122555dd97f988
Admin <harunrashid9159@gmail.com>
2025-01-21 22:51:58 +0530

    Code Complete till 2:23

---
+++ app/src/main/java/np/com/bimalkafle/calculator/Calculator.kt
@@ -0,0 +1,32 @@
+package np.com.bimalkafle.calculator
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun Calculator(modifier: Modifier = Modifier){
+    Box(modifier = modifier){
+        Column(
+            modifier=modifier.fillMaxSize(),
+            horizontalAlignment = Alignment.End
+        ) {
+            Text(
+                text = ("123+123"),
+                style = TextStyle(
+                    fontSize = 30.sp,
+                    textAlign = TextAlign.End
+                ),
+            )
+        }
+    }
+
+}
