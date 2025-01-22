7a4975d67ec68f26fc12732a8f79f46c11c586c3
Admin <harunrashid9159@gmail.com>
2025-01-22 14:57:09 +0530

    Code Complete till 6:36

--- app/src/main/java/np/com/bimalkafle/calculator/Calculator.kt
+++ app/src/main/java/np/com/bimalkafle/calculator/Calculator.kt
@@ -1,22 +1,26 @@
 package np.com.bimalkafle.calculator
 
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import org.w3c.dom.Text
 
 
 val buttonList = listOf(
@@ -56,11 +60,33 @@ fun Calculator(modifier: Modifier = Modifier){
             LazyVerticalGrid(
                 columns = GridCells.Fixed(4),
             ) {
-                items(buttonList){
-                    Text(text = it)
+                item(buttonList){
+                    CalculatorButton(btn = it)
                 }
             }
         }
     }
+
+}
+
+@Composable
+fun CalculatorButton(btn : String) {
+    Box(modifier = Modifier.padding(8.dp)){
+        FloatingActionButton(
+            onClick = { },
+            modifier = Modifier.size(80.dp),
+            shape = CircleShape,
+            contentColor = Color.White,
+            containerColor = Color.Red
+            ) {
+            Text(text = btn)
+        }
+        }
+
+    }
+
+fun getColor(btn : String) : Color{
+    if(btn == "C"  || btn =="AC")
+        return Color(0xFFF44336)
 
 }
