ca2615d807d848686ea91ed0a1a0597d707cfc88
Admin <harunrashid9159@gmail.com>
2025-01-22 22:38:34 +0530

    Code Complete till 8:06

--- app/src/main/java/np/com/bimalkafle/calculator/Calculator.kt
+++ app/src/main/java/np/com/bimalkafle/calculator/Calculator.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.FloatingActionButton
@@ -16,6 +17,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
@@ -47,6 +49,8 @@ fun Calculator(modifier: Modifier = Modifier){
                 maxLines = 5,
                 overflow = TextOverflow.Ellipsis
             )
+            Spacer(modifier = Modifier.weight(1f))
+
             Text(
                 text = "246",
                 style = TextStyle(
@@ -60,7 +64,7 @@ fun Calculator(modifier: Modifier = Modifier){
             LazyVerticalGrid(
                 columns = GridCells.Fixed(4),
             ) {
-                item(buttonList){
+                items(buttonList){
                     CalculatorButton(btn = it)
                 }
             }
@@ -77,16 +81,19 @@ fun CalculatorButton(btn : String) {
             modifier = Modifier.size(80.dp),
             shape = CircleShape,
             contentColor = Color.White,
-            containerColor = Color.Red
+            containerColor = getColor(btn)
             ) {
-            Text(text = btn)
+            Text(text = btn, fontSize = 22.sp, fontWeight = FontWeight.Bold)
         }
         }
-
     }
 
 fun getColor(btn : String) : Color{
     if(btn == "C"  || btn =="AC")
         return Color(0xFFF44336)
-
+    if(btn == "(" || btn == ")")
+        return Color.Gray
+    if(btn == "/" || btn == "*" || btn == "+" || btn == "-" || btn == "=")
+        return Color(0xFFFF9800)
+    return Color(0xFF00C8C9)
 }
